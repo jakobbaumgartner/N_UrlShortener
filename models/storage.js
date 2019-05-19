3a1b9ad91f38df68ada3bb760246575aaba7d235
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-05-19 23:22:15 +0200

    first update

---
+++ models/storage.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const mysql = require('mysql2')
+
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('jnotes_db', 'root', 'brun13cajt', {
+  host: 'localhost',
+  dialect: 'mysql' 
+});
+
+const url = sequelize.define('url', {
+	// attributes
+	longurl: {
+	  type: Sequelize.STRING,
+	  allowNull: false
+	},
+	shorturl: {
+	  type: Sequelize.STRING,
+	  allowNull: false
+	  // allowNull defaults to true
+	}
+  }, {
+	// options
+  });
+
+module.exports.sequelize = sequelize
+module.exports.url = url
