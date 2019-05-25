a3bf74f0b1201cc0b357114cc59260dacdbd1dbf
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-05-25 02:28:08 +0200

    figuring out new Promises construction

--- controllers/control.js
+++ controllers/control.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const dnsPromises = require('dns').promises;
+//const dnsPromises = require('dns').promises;
 const app = express()
 const dns = require('dns');
 const { url, sequelize } = require('../models/storage')
 var d = new Date();
+
 
 
 
@@ -12,70 +13,45 @@ module.exports.first = (req, res) => {
 
 }
 
-module.exports.check = (req, res) => {
+module.exports.check =(req, res)  => {
 
-	var longurl = req.body.lurl
+//	var longurl = req.body.lurl
 	var addressIP = ''
+	var goonthen;
 
 	// checks if address as given is already in DB
 
-	url.findOne({
-		where: {
-			longurl: longurl
-		},
-		raw: true
-	}).then(result => {
-		if (result != null) {
-			console.log("Found LongURL.")
-			return ['address', 'DB']
-		}
-	}).catch(err => {
-		console.log(err)
-		return ['DB', 'ERR']
-	})
+	checkfulladdress(req.body.lurl).then(x => {console.log("THENRREENNN")})
+	console.log("Function EXITED.")
+	
+	// if(!goonthen[0]) {return goonthen}
 
-	// checks if url exists using dns 
+	// // checks if url exists using dns 
 
-	dns.lookup(longurl, (err, address, family) => {
-		if (err != null) {
+	// goonthen = await checkDNS (req.body.lurl)
 
-			return ['no', 'ERR']
-		}
+	// if(!goonthen[0]) {return goonthen}
 
-		else {
+	// // check if address as IP is already in DB
 
-			addressIP = address
-			console.log(addressIP + " DNS found.")
-		}
-	});
-	// address: "192.0.43.8" family: IPv4
+	// goonthen = await checkIPaddress (goonthen[1])
 
-	url.findOne({
-		where: {
-			IPaddress: addressIP
-		},
-		raw: true
-	}).then(result => {
-		if (result != null) {
-			return ['result.shorturl', 'DB']
-		}
-	}).catch(err => {
-		console.log(err)
-		return ['DB', 'ERR']
-	})
+	// if(!goonthen[0]) {return goonthen}
 
-	url.create({longurl: longurl, shorturl: d.getTime(), IPaddress: addressIP} )
-	.then(result => {
-		console.log("New address added.")
-		return [result.shorturl, 'NEW']
-	})
+	// // creates new input
+
+
+	// goonthen = await createnewaddress (goonthen[1], req.body.lurl)
+
+
+
 
 
 
 	res.redirect('/');
 }
 
-function checkfulladress () {
+function checkfulladdress (longurl) {
 		// checks if address as given is already in DB
 
 		url.findOne({
@@ -83,15 +59,71 @@ function checkfulladress () {
 				longurl: longurl
 			},
 			raw: true
+			
+		})
+		.then(() => {
+			//if (result != null) {
+				console.log("Found LongURL.")
+				return new Promise ( (resolve) => resolve([false, 'address', 'DB'])) 
+			// }
+			// else {
+			// 	console.log("Not Found LongURL.")
+			// 	return new Promise(resolve => resolve([true]))
+			// }
+			
+		})
+	
+}
+
+
+function checkDNS (longurl) {
+	// checks if url exists using dns 
+	console.log("DNS check initiated.")
+	dns.lookup(longurl, (err, address, family) => {
+		if (err != null) {
+			console.log('DNS not found.')
+			return new Promise(resolve => resolve([false, 'no', 'ERR'])) 
+
+		}
+
+		else {
+
+			console.log(addressIP + " DNS found.")
+			return new Promise(resolve => resolve([true, address])) 
+
+		}
+	});
+}
+
+function checkIPaddress (addressIP) {
+		// check if address as IP is already in DB
+
+		url.findOne({
+			where: {
+				IPaddress: addressIP
+			},
+			raw: true
 		}).then(result => {
 			if (result != null) {
-				console.log("Found LongURL.")
-				return ['address', 'DB']
+				return [false, 'result.shorturl', 'DB']
 			}
-			
+			else {
+				return [true, addressIP]
+			}
 		}).catch(err => {
 			console.log(err)
 			return ['DB', 'ERR']
+		})
+	
+}
+
+function createnewaddress (addressIP, longurl) {
+		// creates new input
+
+		url.create({longurl: longurl, shorturl: d.getTime(), IPaddress: addressIP} )
+		.then(result => {
+			console.log("New address added.")
+			return [false, result.shorturl, addressIP, 'NEW']
 		})
 	
 }
